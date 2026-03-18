e0169f0ddafbcbf6daf58e08295b766ed8d02359
sandhya murali <sandhyamuralinov26@gmail.com>
2026-03-18 14:55:38 +0100

    Added POJO class for ListOfRooms and Rooms

---
+++ src/test/java/pojo/ListRoomDetails.java
@@ -0,0 +1,19 @@
+package pojo;
+
+import java.util.List;
+
+public class ListRoomDetails {
+    private List<Rooms> rooms;
+
+    public List<Rooms> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(List<Rooms> rooms) {
+        this.rooms = rooms;
+    }
+
+
+
+
+}
---
+++ src/test/java/pojo/Rooms.java
@@ -0,0 +1,85 @@
+package pojo;
+
+import java.util.List;
+
+public class Rooms {
+
+    private int roomid;
+    private String roomName;
+
+    private String type;
+    private boolean accessible;
+    private String image;
+    private String description;
+    private List<String> features;
+    private int roomPrice;
+
+    public int getRoomid() {
+        return roomid;
+    }
+
+    public void setRoomid(int roomid) {
+        this.roomid = roomid;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean getAccessible() {
+        return accessible;
+    }
+
+    public void setAccessible(boolean accessible) {
+        this.accessible = accessible;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<String> features) {
+        this.features = features;
+    }
+
+    public int getRoomPrice() {
+        return roomPrice;
+    }
+
+    public void setRoomPrice(int roomPrice) {
+        this.roomPrice = roomPrice;
+    }
+
+
+
+
+
+}
