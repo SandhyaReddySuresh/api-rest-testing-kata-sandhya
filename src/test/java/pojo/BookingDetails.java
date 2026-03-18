c7ddf62df867fa7209d1b7e9811d9d2a6d4ac48b
sandhya murali <sandhyamuralinov26@gmail.com>
2026-03-18 22:08:12 +0100

    Added Booking Details pojo class added

---
+++ src/test/java/pojo/BookingDetails.java
@@ -0,0 +1,16 @@
+package pojo;
+
+import java.util.List;
+
+public class BookingDetails {
+    private List<BookingResponse> bookings;
+
+    public List<BookingResponse> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<BookingResponse> bookings) {
+        this.bookings = bookings;
+    }
+
+}
