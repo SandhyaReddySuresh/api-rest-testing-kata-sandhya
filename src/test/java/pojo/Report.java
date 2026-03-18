5dffe29c6d7370da8cc59e752c8490c761d274fd
sandhya murali <sandhyamuralinov26@gmail.com>
2026-03-18 23:31:48 +0100

    Added Report and ReportSummary pojo

---
+++ src/test/java/pojo/Report.java
@@ -0,0 +1,32 @@
+package pojo;
+
+public class Report {
+    private String start;
+    private String end;
+    private String title;
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
---
+++ src/test/java/pojo/ReportSummary.java
@@ -0,0 +1,15 @@
+package pojo;
+
+import java.util.List;
+
+public class ReportSummary {
+    private List<Report> report;
+
+    public List<Report> getReport() {
+        return report;
+    }
+
+    public void setReport(List<Report> report) {
+        this.report = report;
+    }
+}
