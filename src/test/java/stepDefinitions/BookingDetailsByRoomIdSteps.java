e180196924a126b4a94169518aab44e6b4347617
sandhya murali <sandhyamuralinov26@gmail.com>
2026-03-19 05:27:31 +0100

    Added Steps in BookingDetailsByRoomId

---
+++ src/test/java/stepDefinitions/BookingDetailsByRoomIdSteps.java
@@ -0,0 +1,38 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class BookingDetailsByRoomIdSteps {
+
+    @When("the customer selects room {string} to view booking details")
+    public void theCustomerSelectsRoomToViewBookingDetails(String roomId) {
+        
+        
+    }
+
+    @Then("the booking details should be displayed successfully")
+    public void theBookingDetailsShouldBeDisplayedSuccessfully() {
+        
+    }
+
+    @And("the booking should include:")
+    public void theBookingShouldInclude() {
+        
+    }
+
+    @When("the customer selects a room with no bookings to check booking details")
+    public void theCustomerSelectsARoomWithNoBookingsToCheckBookingDetails() {
+        
+    }
+
+    @Then("the system should show {string}")
+    public void theSystemShouldShow(String arg0) {
+        
+    }
+
+    @When("the customer selects an invalid room to check booking details")
+    public void theCustomerSelectsAnInvalidRoomToCheckBookingDetails() {
+    }
+}
--- src/test/java/stepDefinitions/GetBookingReportSteps.java
+++ src/test/java/stepDefinitions/GetBookingReportSteps.java
@@ -25,4 +25,6 @@ public class GetBookingReportSteps {
         GetBookingReportAPI.getBookingReportSummaryAPI_Response();
 
     }
+
+
 }
